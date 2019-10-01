e3feefb8bc17a8a27ef52a7d26d01b5ded58c328
Jshoren <54953242+Jshoren@users.noreply.github.com>
2019-10-01 15:45:16 -0400

    Update help.js

--- help.js
+++ help.js
@@ -16,6 +16,29 @@ function check(){
 		correct++;
 	}
 	
+	var pictures = ["img/win.gif", "img/meh.jpeg", "img/lose.gif"];
+	var messages = ["Great job!", "That's just okay", "You really need to do better"];
+	var score;
+
+	if (correct == 0) {
+		score = 2;
+	}
+
+	if (correct > 0 && correct < 3) {
+		score = 1;
+	}
+
+	if (correct == 3) {
+		score = 0;
+	}
+
+	document.getElementById("after_submit").style.visibility = "visible";
+
+	document.getElementById("message").innerHTML = messages[score];
+	document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
+	document.getElementById("picture").src = pictures[score];
+	}
+	
 	
 	
 	
